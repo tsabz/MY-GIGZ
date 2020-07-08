6ae46fcb367dc95b09f79009f7b8236ca70d01f9
Roy Sanfilipo <roysanfilipo@gmail.com>
2020-07-08 13:59:15 -0400

    updated congroller and model and htaccess files

---
+++ controllers/gigz.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once __DIR__ . '/../models/gig.php';
+header('Content-Type: application/json');
+if ($_REQUEST['action'] === 'index') {
+  echo json_encode(Gigz::all());
+} elseif ($_REQUEST['action'] === 'post') {
+  $request_body = file_get_contents('php://input');
+  $body_object = json_decode($request_body);
+  $new_gig = new Gig(null, $body_object->name, $body_object->date, $body_object->location, $body_object->compensation, $body_object->notes);
+  $all_gigz = Gigz::create($new_gig);
+  echo json_encode($all_gigz);
+} else if ($_REQUEST['action'] === 'update'){
+  $request_body = file_get_contents('php://input');
+  $body_object = json_decode($request_body);
+  $updated_post = new Gig($_REQUEST['id'], $body_object->name, $body_object->date, $body_object->location, $body_object->compensation, $body_object->notes);
+  $all_posts = Gigz::update($updated_gig);
+  echo json_encode($all_gigz);
+  } else if ($_REQUEST['action'] === 'delete') {
+    $all_posts = Gigz::delete($_REQUEST['id']);
+    echo json_encode($all_gigz);
+  }
+
+ ?>
--- models/gig.php
+++ models/gig.php
@@ -1,18 +1,69 @@
-$dbconn = null;
-if(getenv('DATABASE_URL')){
-$connectionConfig = parse_url(getenv('DATABASE_URL'));
-$host = $connectionConfig['host'];
-$user = $connectionConfig['user'];
-$password = $connectionConfig['pass'];
-$port = $connectionConfig['port'];
-$dbname = trim($connectionConfig['path'],'/');
-$dbconn = pg_connect(
-"host=".$host." ".
-"user=".$user." ".
-"password=".$password." ".
-"port=".$port." ".
-"dbname=".$dbname
-);
-} else {
-$dbconn = pg_connect("host=localhost dbname=phpapi");
+
+<?php
+$dbconn = pg_connect("host=localhost dbname=mygigz");
+
+class Gig {
+  public $id;
+  public $name;
+  public $date;
+  public $location;
+  public $compensation;
+  public $notes;
+
+
+  public function __construct($id, $name, $date, $location, $compensation, $notes){
+    $this->id = $id;
+    $this->name = $name;
+    $this->date = $date;
+    $this->location = $location;
+    $this->compensation = $compensation;
+    $this->notes = $notes;
+  }
 }
+
+class Gigz {
+  static function all(){
+    $gigz = array();
+
+    $results = pg_query("SELECT * FROM gigz");
+
+    $row_object = pg_fetch_object($results);
+    while($row_object){
+      $new_gig = new Gig(
+        intval($row_object->id),
+        $row_object->name,
+        $row_object->date,
+        $row_object->location,
+        $row_object->compensation,
+        $row_object->notes
+      );
+      $gigz[] = $new_gig;
+      $row_object = pg_fetch_object($results);
+    }
+    return $gigz;
+  }
+
+  static function create($gig){
+    $query = "INSERT INTO gigz (name, date, location, compensation, notes) VALUES ($1, $2, $3, $4, $5)";
+    $query_params = array($gig->name, $gig->date, $gig->location, $gig->compensation, $gig->notes);
+    pg_query_params($query, $query_params);
+    return self::all();
+  }
+
+  static function update($updated_gig){
+      $query = "UPDATE gigz SET name = $1, date = $2, location = $3, compensation = $4, notes = $5 WHERE id = $6";
+      $query_params = array($updated_gig->name, $updated_gig->date, $updated_gig->location, $updated_gig->compensation, $updated_gig->notes, $updated_gig->id);
+      $result = pg_query_params($query, $query_params);
+
+      return self::all();
+    }
+    static function delete($id){
+      $query = "DELETE FROM gigz WHERE id = $1";
+      $query_params = array($id);
+      $result = pg_query_params($query, $query_params);
+
+      return self::all();
+    }
+}
+
+ ?>
