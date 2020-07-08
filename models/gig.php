6062010806fd94cad245caf42d2d529de93ff1a7
Roy Sanfilipo <roysanfilipo@gmail.com>
2020-07-08 12:10:27 -0400

    added composer file and fixed css file structure

---
+++ models/gig.php
@@ -0,0 +1,18 @@
+$dbconn = null;
+if(getenv('DATABASE_URL')){
+$connectionConfig = parse_url(getenv('DATABASE_URL'));
+$host = $connectionConfig['host'];
+$user = $connectionConfig['user'];
+$password = $connectionConfig['pass'];
+$port = $connectionConfig['port'];
+$dbname = trim($connectionConfig['path'],'/');
+$dbconn = pg_connect(
+"host=".$host." ".
+"user=".$user." ".
+"password=".$password." ".
+"port=".$port." ".
+"dbname=".$dbname
+);
+} else {
+$dbconn = pg_connect("host=localhost dbname=phpapi");
+}
